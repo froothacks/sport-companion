f0a1019e5f8ab6b28c16dbeaff044443491c48b7
Advait-M <advait.maybhate9@gmail.com>
2018-10-13 12:14:25 -0400

    Schedules calls

--- src-twilio/make_call.py
+++ src-twilio/make_call.py
@@ -1,6 +1,10 @@
 from twilio.rest import Client
 import urllib.parse
 import json
+import os, time
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime, timedelta
+import dateutil.parser
 
 # Load secrets
 with open('secrets.json') as secretsFile:
@@ -17,10 +21,28 @@ message = urllib.parse.quote_plus(message)
 print(message)
 
 # Create call from desired number to specified number (from secrets)
-call = client.calls.create(
-                        url='https://handler.twilio.com/twiml/EH2cf9a2f6ab735976090d24e9ca654d96?Message='+message,
-                        to=secrets["to"],
-                        from_=secrets["from"]
-                    )
-# Print call ID
-print(call.sid)
+def call(callMessage):
+	call = client.calls.create(
+        url='https://handler.twilio.com/twiml/EH2cf9a2f6ab735976090d24e9ca654d96?Message='+callMessage,
+        to=secrets["to"],
+        from_=secrets["from"]
+    )
+
+	# Print call ID
+	print(call.sid)
+
+def tick(text):
+    print(text + '! The time is: %s' % datetime.now())
+
+
+scheduler = BackgroundScheduler()
+dd = dateutil.parser.parse("2018-10-13T12:11:50")
+scheduler.add_job(call, 'date',run_date=dd, args=[message])
+
+scheduler.start()
+try:
+    # Simulate application activity
+    while True:
+        time.sleep(2)
+except (KeyboardInterrupt, SystemExit):
+    scheduler.shutdown()
